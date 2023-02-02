62f4ed2622bebe4e7822d5c2dacf6d6a82aca767
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-02 22:45:23 +0200

    add form with cities, 1 route to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit, OnDestroy{
+
+  cities = ["London", "Paris", "Kyiv", "New York", "Sydney", "Zaporizhzhia"];
+
+  cityControl: FormControl;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.cityControl = new FormControl("");
+    this.cityControl.valueChanges
+      .subscribe(value => {
+        this.router.navigate([value]);
+      });
+  }
+
+  ngOnDestroy(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {Route, RouterModule} from "@angular/router";
+import { WeatherReportComponent } from './components/weather-report/weather-report.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatSelectModule} from "@angular/material/select";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule} from "@angular/material/button";
+
+const routes: Route[] = [
+  {path: ':locationName', component: WeatherReportComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherReportComponent,
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
