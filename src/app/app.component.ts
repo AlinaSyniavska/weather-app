12547eb076971b641a193db2d5fcee56a1288b6e
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-03 23:56:19 +0200

    adding conditional select fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,55 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {Router} from "@angular/router";
+import {map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit, OnDestroy {
 
-  cities = ["London", "Paris", "Kyiv", "New York", "Sydney", "Zaporizhzhia", "Irpin"];
+  countries = [
+    {
+      name: "United Kingdom",
+      cities: ["London", "Warwick", "Birmingham"]
+    },
+    {
+      name: "United States",
+      cities: ["New York", "Chicago", "Washington"]
+    },
+    {
+      name: "Australia",
+      cities: ["Sydney", "Adelaide", "Melbourne"]
+    },
+    {
+      name: "Ukraine",
+      cities: ["Kyiv", "Zaporizhzhia", "Irpin", "Lviv", "Lutsk"]
+    }
+  ];
 
+  cities: Observable<string>;
+
+  countryControl: FormControl;
   cityControl: FormControl;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+    this.countryControl = new FormControl("");
     this.cityControl = new FormControl("");
 
     this.cityControl.valueChanges
       .subscribe(value => {
         this.router.navigate([value]);
       });
+
+    this.cities = this.countryControl.valueChanges.pipe(
+      map(country => country.cities)
+    );
+
   }
 
   ngOnDestroy(): void {
